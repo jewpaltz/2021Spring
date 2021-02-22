1719c532b1a66a6501df3c09b62203880f480970
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-02-22 16:45:31 -0500

    import bulma, buefy, fortAwesome

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bulma';
+import 'buefy/dist/buefy.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './sass/main.scss';
+
 Vue.config.productionTip = false
 
 new Vue({
