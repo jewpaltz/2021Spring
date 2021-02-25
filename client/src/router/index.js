45ecd38d709abddfdc4697c9aedfad1e0a4e01a3
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-02-25 15:29:34 -0500

    Routing and Composition

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,22 +1,22 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Documentation from '@/views/Documentation';
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+  { path: '/', name: 'Home', component: Home },
+  { path: '/documentation', name: 'Doc', component: Documentation },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "secondary" */ '../views/About.vue')
+  },
+  {
+    path: '/shop',
+    name: 'shop',
+    component: () => import(/* webpackChunkName: "secondary" */ '../views/Shop.vue')
   }
 ]
 
