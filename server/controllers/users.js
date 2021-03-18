fee3b7a627c0a0b8deed55d4af5ca97792bf9e71
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-03-18 14:26:52 -0400

    Complete REST controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,6 +7,29 @@ const model = require('../models/users');
 const app = express.Router();
 
     app
-        .get('/', (req, res)=> res.send( model.GetAll() ) )
+        .get('/', (req, res)=>{
+            res.send( model.GetAll() );
+            console.log(req.headers);            
+        }  )
+        .get('/:user_id',(req, res)=> res.send( model.Get(req.params.user_id) ))
+        .post('/', (req, res)=> { 
+            res.send( model.Add({
+                firstName: req.body.firstName,
+                lastName:  req.body.lastName,
+                handle: req.body.handle,
+                pic:  req.body.pic,
+            }) );
+            console.log(req.headers);
+        })
+        .patch('/:user_id', (req, res)=> res.send( model.Update(
+            req.params.user_id,
+            {
+                firstName: req.body.firstName,
+                lastName:  req.body.lastName,
+                handle: req.body.handle,
+                pic:  req.body.pic,
+        }) ) )
+        .delete('/:user_id', (req, res)=> res.send( model.Delete(req.params.user_id) ) )
+
 
 module.exports = app;
--- server/models/users.js
+++ server/models/users.js
@@ -3,7 +3,58 @@
 */
 
 const list = [
-    { name: 'Moshe Plotkin'}
+    { 
+        firstName: 'Moshe',
+        lastName: 'Plotkin',
+        handle: '@JewPaltz',
+        pic: 'https://bulma.io/images/placeholders/96x96.png',
+        password: 'Me',
+    },
+    { 
+        firstName: 'Kamala',
+        lastName: 'Harris',
+        handle: '@vpotus',
+        pic: 'https://bulma.io/images/placeholders/96x96.png',
+        password: 'Her',
+    },
+    { 
+        firstName: 'John',
+        lastName: 'Smith',
+        handle: '@johnsmith',
+        pic: 'https://bulma.io/images/placeholders/96x96.png',
+        password: 'BeepBop',
+    },
+
 ];
 
 module.exports.GetAll = ()=> list;
+module.exports.Get = (user_id)=> list[user_id];
+module.exports.Add = (user)=> {
+    if(!user.firstName){
+        throw "First Name is reqired"
+    }
+     list.push(user);
+     return { ...user, password: undefined };
+}
+module.exports.Update = (user_id, user)=> {
+    const oldObj = list[user_id];
+    if(user.firstName){
+        oldObj.firstName = user.firstName;
+    }
+    if(user.lastName){
+        oldObj.lastName = user.lastName;
+    }
+    if(user.handle){
+        oldObj.handle = user.handle;
+    }
+    if(user.pic){
+        oldObj.pic = user.pic;
+    }
+    //list[user_id] = newObj ;
+    return { ...oldObj, password: undefined };
+}
+module.exports.Delete = (user_id)=> {
+    const user = list[user_id];
+    list.splice(user_id, 1);
+    return user;
+}
