5710619b8d92eae19bd169ad5edb72ed7771382d
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-03-25 14:13:01 -0400

    error handling

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -13,23 +13,12 @@ const app = express.Router();
         }  )
         .get('/:user_id',(req, res)=> res.send( model.Get(req.params.user_id) ))
         .post('/', (req, res)=> { 
-            res.send( model.Add({
-                firstName: req.body.firstName,
-                lastName:  req.body.lastName,
-                handle: req.body.handle,
-                pic:  req.body.pic,
-            }) );
-            console.log(req.headers);
-            console.log(req.body);
+            res.send( model.Add(req.body) );
         })
-        .patch('/:user_id', (req, res)=> res.send( model.Update(
-            req.params.user_id,
-            {
-                firstName: req.body.firstName,
-                lastName:  req.body.lastName,
-                handle: req.body.handle,
-                pic:  req.body.pic,
-        }) ) )
+        .post('/login', (req, res)=> { 
+            res.send( model.Login(req.body.handle, req.body.password) )
+        })
+        .patch('/:user_id', (req, res)=> res.send( model.Update( req.params.user_id, req.body ) ) )
         .delete('/:user_id', (req, res)=> res.send( model.Delete(req.params.user_id) ) )
 
 
--- server/models/users.js
+++ server/models/users.js
@@ -32,7 +32,7 @@ module.exports.Get = (user_id)=> list[user_id];
 module.exports.GetByHandle = (handle)=> ({ ...list.find( (x, i)=> x.handle == handle ), password: undefined }) ;
 module.exports.Add = (user)=> {
     if(!user.firstName){
-        throw "First Name is reqired"
+        throw { code: 422, msg: "First Name is reqired" }
     }
      list.push(user);
      return { ...user, password: undefined };
@@ -57,5 +57,13 @@ module.exports.Update = (user_id, user)=> {
 module.exports.Delete = (user_id)=> {
     const user = list[user_id];
     list.splice(user_id, 1);
+    return user;
+}
+
+module.exports.Login = (handle, password) =>{
+    console.log({ handle, password})
+    const user = list.find(x=> x.handle == handle && x.password == password);
+    if(!user) throw { code: 401, msg: "Wrong Username or Password" };
+
     return user;
 }
