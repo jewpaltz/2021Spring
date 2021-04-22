edc3ada3f875578de8faef30661b90ccddc5488a
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-04-22 12:09:49 -0400

    Only loading main js sdk file

--- client/src/App.vue
+++ client/src/App.vue
@@ -23,19 +23,23 @@ export default {
 }
         /*global FB*/
         window.fbAsyncInit = function() {
+          console.log('FB.init')
           FB.init({
             xfbml            : true,
             version          : 'v10.0'
           });
         };
+        console.log('window.fbAsyncInit');
 
-        (function(d, s, id) {
+        function loadFB(d, s, id, url) {
           var js, fjs = d.getElementsByTagName(s)[0];
           if (d.getElementById(id)) return;
           js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+          js.src = url;
           fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+        }
+        loadFB(document, 'script', 'facebook-jssdk1', 'https://connect.facebook.net/en_US/sdk.js');
+        //loadFB(document, 'script', 'facebook-jssdk2', 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js');
 
 </script>
 
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,35 +1,45 @@
 <template>
-    <form @submit.prevent="login">
-        <div class="field">
-        <p class="control has-icons-left has-icons-right">
-            <input class="input" type="text" placeholder="Handle" v-model="handle">
-            <span class="icon is-small is-left">
-            <i class="fas fa-envelope"></i>
-            </span>
-            <span class="icon is-small is-right">
-            <i class="fas fa-check"></i>
-            </span>
-        </p>
-        </div>
-        <div class="field">
-        <p class="control has-icons-left">
-            <input class="input" type="password" placeholder="Password" v-model="password">
-            <span class="icon is-small is-left">
-            <i class="fas fa-lock"></i>
-            </span>
-        </p>
-        </div>
-        <div class="field">
-        <p class="control">
-            <button class="button is-success">
-            Login
+<div class="collumns">
+    <div class="column is-half is-offset-one-quarter">
+        <form @submit.prevent="login" class="box">
+            <div class="field">
+            <p class="control has-icons-left has-icons-right">
+                <input class="input" type="text" placeholder="Handle" v-model="handle">
+                <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+                </span>
+                <span class="icon is-small is-right">
+                <i class="fas fa-check"></i>
+                </span>
+            </p>
+            </div>
+            <div class="field">
+            <p class="control has-icons-left">
+                <input class="input" type="password" placeholder="Password" v-model="password">
+                <span class="icon is-small is-left">
+                <i class="fas fa-lock"></i>
+                </span>
+            </p>
+            </div>
+            <div class="field">
+            <p class="control">
+                <button class="button is-success">
+                Login
+                </button>
+            </p>
+            </div>
+
+            <button class="button is-primary is-large is-fullwidth" @click.prevent="loginFB">
+                Login with Facebook
             </button>
-        </p>
-        </div>
-    </form>
+        </form>
+        
+    </div>
+</div>
 </template>
 
 <script>
+import { ToastProgrammatic as toastr } from "buefy";
 import { Login } from "../models/Session";
 export default {
     data: ()=>({
@@ -39,6 +49,20 @@ export default {
     methods: {
         login(){
             Login(this.handle, this.password);
+        },
+        loginFB(){
+            /*global FB*/
+            FB.login(function(response) {
+                console.log({response})
+                if (response.status === 'connected') {
+                    toastr.open({
+                        type: 'is-success',
+                        message: `Welcome ${response.name}`
+                    })
+                } else {
+                    // The person is not logged into your webpage or we are unable to tell. 
+                }
+            }, {scope: 'public_profile,email'});
         }
     }
 }
