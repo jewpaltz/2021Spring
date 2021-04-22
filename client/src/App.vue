f679a7f81adcad7634c80a079218df7c49488140
Moshe Plotkin <mplotkin@jewpaltz.com>
2021-04-22 11:41:59 -0400

    chat plugin

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,7 +5,11 @@
       <router-view/>
     </div>
 
-
+      <div id="fb-root"></div>
+      <div class="fb-customerchat"
+        attribution="biz_inbox"
+        page_id="138346359546785">
+      </div>
   </div>
 </template>
 
@@ -17,6 +21,22 @@ export default {
     MyNav
   }
 }
+        /*global FB*/
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
 </script>
 
 <style lang="scss">
